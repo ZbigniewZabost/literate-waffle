02d71f205ceba647245f349105d6c5bd7a20be79
zbigniewz <zabostz@gmail.com>
2016-03-12 11:53:44 +0100

    fix pylint errors

--- indexer/indexer.py
+++ indexer/indexer.py
@@ -7,13 +7,16 @@ from elasticsearch import Elasticsearch, helpers
 
 def create_parser():
     parser = argparse.ArgumentParser(description='Index words to elasticsearch.')
-    parser.add_argument('--es_host', default='localhost', help='Hostname of elasticsearch, default = localhost')
-    parser.add_argument('--es_port', type=int, default=9200, help='Hostname of elasticsearch, default = 9200')
+    parser.add_argument('--es_host', default='localhost', help='Hostname of elasticsearch, default'
+                                                               ' = localhost')
+    parser.add_argument('--es_port', type=int, default=9200, help='Hostname of elasticsearch, '
+                                                                  'default = 9200')
     parser.add_argument('--index', default='german', help='Name of index, default = german')
     parser.add_argument('--type', default='word', help='Type of indexed documents, default = word')
     parser.add_argument('--dictionary', default='data/german.dic',
                         help='Path to dictionary file, default = data/german.dic')
-    parser.add_argument('--dict_encoding', default='latin-1', help='Dictionary file encoding, default = latin-1')
+    parser.add_argument('--dict_encoding', default='latin-1', help='Dictionary file encoding, '
+                                                                   'default = latin-1')
     parser.add_argument('--delete_index', action='store_true', default=False,
                         help='Drop index before indexing?, default = False')
     return parser
@@ -50,14 +53,14 @@ def build_es_action(es_index, doc_type, doc_id, doc):
 
 def setup_es(host, port, es_index, delete_index):
     print 'Setting up connection to elasticsearch on %s:%s' % (host, port)
-    es = Elasticsearch([{'host': host, 'port': port}])
+    es_connection = Elasticsearch([{'host': host, 'port': port}])
     if delete_index:
         print 'Deleting index %s...' % es_index
-        es.indices.delete(es_index)
+        es_connection.indices.delete(es_index)
     # ignore 400 cause by IndexAlreadyExistsException when creating an index
     print 'Creating index %s...' % es_index
-    es.indices.create(index=index, ignore=400)
-    return es
+    es_connection.indices.create(index=index)
+    return es_connection
 
 
 def print_progress(indexed_so_far, all_docs):
@@ -65,7 +68,7 @@ def print_progress(indexed_so_far, all_docs):
     print 'Indexed documents: %d from %d, progress: %.f%%' % (indexed_so_far, all_docs, percent)
 
 
-def index(args, es):
+def index(args, es_conn):
     with open(args.dictionary) as dict_file:
         print 'Counting number of lines in %s ...' % dict_file.name
         num_lines = sum(1 for line in dict_file)
@@ -80,21 +83,21 @@ def index(args, es):
             indexed_counter += 1
             if len(es_actions) >= 500:
                 print "Indexing batch..."
-                helpers.bulk(es, es_actions)
+                helpers.bulk(es_conn, es_actions)
                 es_actions = []
                 print_progress(indexed_counter, num_lines)
 
         # index remaining docs
         print "Indexing batch..."
-        helpers.bulk(es, es_actions)
+        helpers.bulk(es_conn, es_actions)
         print_progress(indexed_counter, num_lines)
 
 
 def main():
     parser = create_parser()
     args = parser.parse_args()
-    es = setup_es(args.es_host, args.es_port, args.index, args.delete_index)
-    index(args, es)
+    es_conn = setup_es(args.es_host, args.es_port, args.index, args.delete_index)
+    index(args, es_conn)
 
 
 if __name__ == '__main__':
