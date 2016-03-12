3cc2bb737e7d151d519e6e26e46ab4ac088762ae
zbigniewz <zabostz@gmail.com>
2016-03-12 09:27:01 +0100

    add more info to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from distutils.core import setup
 setup(
     name='Literate Waffle',
     version='0.1',
-    packages=['indexer',],
     license='Apache License Version 2.0',
-    long_description=open('README.md').read(), requires=['elasticsearch']
+    long_description=open('README.md').read(),
+    author='zbigniewz',
+    url='https://github.com/zbigniewz/literate-waffle',
+    requires=['elasticsearch'],
+    packages=['indexer']
 )
